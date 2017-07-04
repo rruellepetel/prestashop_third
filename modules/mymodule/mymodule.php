eea47394b544c127865f6a6c478e7e4da5266374
rruellepetel <ruellepetelromain@gmail.com>
2017-07-04 16:19:03 +0200

    fix require php andd add config/ in .gitignore

--- modules/mymodule/mymodule.php
+++ modules/mymodule/mymodule.php
@@ -1,62 +1,18 @@
 <?php
-if (!defined('_PS_VERSION_'))
-  exit;
 
-class MyModule extends Module
-{
-  public function __construct()
-  {
-    $this->name = 'mymodule';
-    $this->tab = 'front_office_features';
-    $this->version = '1.0.0';
-    $this->author = 'Romain';
-    $this->need_instance = 0;
-    $this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
-    $this->bootstrap = true;
 
-    parent::__construct();
-
-    $this->displayName = $this->l('My module');
-    $this->description = $this->l('Awesome module.');
-
-    $this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
-
-    if (!Configuration::get('MYMODULE_NAME'))
-      $this->warning = $this->l('No name provided');
-  }
-  public function install()
-  {
-    if (Shop::isFeatureActive())
-    Shop::setContext(Shop::CONTEXT_ALL);
-
-    if (!parent::install() ||
-    !$this->registerHook('leftColumn') ||
-    !$this->registerHook('header') ||
-    !Configuration::updateValue('MYMODULE_NAME', 'my friend')
-    )
-    return false;
-
-    return true;
-  }
-  public function uninstall()
-  {
-    if (!parent::uninstall() ||
-    !Configuration::deleteByName('MYMODULE_NAME')
-    )
-    return false;
-
-    return true;
-  }
+if (!defined('_PS_VERSION_')) {
+    exit;
+    // // Storing a serialized array.
+    // Configuration::updateValue('MYMODULE_SETTINGS', serialize(array(true, true, false)));
+    //
+    // // Retrieving the array.
+    // $configuration_array = unserialize(Configuration::get('MYMODULE_SETTINGS'));
+    //
+    //
+    //
+    // if (Shop::isFeatureActive()) {
+    //     Shop::setContext(Shop::CONTEXT_ALL);
+    // }
 }
-
-
-// Storing a serialized array.
-Configuration::updateValue('MYMODULE_SETTINGS', serialize(array(true, true, false)));
-
-// Retrieving the array.
-$configuration_array = unserialize(Configuration::get('MYMODULE_SETTINGS'));
-
-
-
-if (Shop::isFeatureActive())
-  Shop::setContext(Shop::CONTEXT_ALL);
+require('classes/MyModule.php');
