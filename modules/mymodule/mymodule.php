fb94222df246ce227e69934af103416d5513a8c0
rruellepetel <ruellepetelromain@gmail.com>
2017-07-04 14:54:10 +0200

    add mymodule.php

--- modules/mymodule/mymodule.php
+++ modules/mymodule/mymodule.php
@@ -9,7 +9,7 @@ class MyModule extends Module
     $this->name = 'mymodule';
     $this->tab = 'front_office_features';
     $this->version = '1.0.0';
-    $this->author = 'Firstname Lastname';
+    $this->author = 'Romain';
     $this->need_instance = 0;
     $this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
     $this->bootstrap = true;
@@ -17,11 +17,46 @@ class MyModule extends Module
     parent::__construct();
 
     $this->displayName = $this->l('My module');
-    $this->description = $this->l('Description of my module.');
+    $this->description = $this->l('Awesome module.');
 
     $this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
 
     if (!Configuration::get('MYMODULE_NAME'))
       $this->warning = $this->l('No name provided');
   }
+  public function install()
+  {
+    if (Shop::isFeatureActive())
+    Shop::setContext(Shop::CONTEXT_ALL);
+
+    if (!parent::install() ||
+    !$this->registerHook('leftColumn') ||
+    !$this->registerHook('header') ||
+    !Configuration::updateValue('MYMODULE_NAME', 'my friend')
+    )
+    return false;
+
+    return true;
+  }
+  public function uninstall()
+  {
+    if (!parent::uninstall() ||
+    !Configuration::deleteByName('MYMODULE_NAME')
+    )
+    return false;
+
+    return true;
+  }
 }
+
+
+// Storing a serialized array.
+Configuration::updateValue('MYMODULE_SETTINGS', serialize(array(true, true, false)));
+
+// Retrieving the array.
+$configuration_array = unserialize(Configuration::get('MYMODULE_SETTINGS'));
+
+
+
+if (Shop::isFeatureActive())
+  Shop::setContext(Shop::CONTEXT_ALL);
